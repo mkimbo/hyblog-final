fb91a794b2f10cfdf82897bc60f3a4613786ee8a
Jack Mkimbo <nickihll@gmail.com>
2021-03-09 22:14:28 +0300

    added particles background

--- src/components/parallax.js
+++ src/components/parallax.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
+import Particles from 'react-tsparticles'
 import Img from 'gatsby-image'
 import PropTypes from 'prop-types'
 
@@ -49,11 +50,75 @@ export default function Parallax(props) {
         transform: transform,
         backgroundColor: '#f7f9fc',
         textAlign: 'center',
+        position: 'relative',
       }}
     >
       <div
         style={{
+          height: '100%',
+        }}
+      >
+        <Particles
+          height="90vh"
+          params={{
+            fpsLimit: 60,
+            interactivity: {
+              detectsOn: 'canvas',
+              events: {
+                resize: true,
+              },
+            },
+            particles: {
+              color: { value: '#110f8b' },
+              links: {
+                color: '#110f8b',
+                distance: 100,
+                enable: true,
+                opacity: 1,
+                width: 1,
+              },
+              number: { value: 100 },
+              move: {
+                enable: true,
+              },
+              opacity: {
+                value: 1,
+                random: true,
+              },
+              shape: {
+                character: {
+                  fill: false,
+                  font: 'Verdana',
+                  style: '',
+                  value: '*',
+                  weight: '400',
+                },
+                image: {
+                  height: 100,
+                  replace_color: true,
+                  src: 'images/github.svg',
+                  width: 100,
+                },
+                polygon: { nb_sides: 5 },
+                type: 'circle',
+              },
+              stroke: { color: '#ffffff', width: 3 },
+              size: {
+                value: 10,
+              },
+            },
+            retina_detect: true,
+          }}
+        />
+      </div>
+      <div
+        style={{
           paddingTop: '25px',
+          background: 'transparent',
+          position: 'absolute',
+          top: '30%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
         }}
       >
         <Img fixed={data.background.childImageSharp.fixed} />
