2a71f07cb00edd7e629eb1a5e5636ae0ba30238f
Jack Mkimbo <nickihll@gmail.com>
2021-03-10 15:38:51 +0300

    added settings in Netlify CMS

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,6 +21,10 @@ import StarIcon from '@material-ui/icons/Star'
 import InfoIcon from '@material-ui/icons/Info'
 import ExpandLess from '@material-ui/icons/ExpandLess'
 import ExpandMore from '@material-ui/icons/ExpandMore'
+import { Card, CardHeader, Avatar, IconButton } from '@material-ui/core'
+
+import aboutHyblog from '../../site/information/about-hyblog.json'
+import aboutHyreads from '../../site/information/about-hyreads.json'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -80,236 +84,150 @@ const About = () => {
       >
         <Grid container spacing={3} justify="center" align="center">
           <Grid item xs={12}>
+            <Card raised>
+              <Typography variant="h4">{aboutHyblog.title}</Typography>
+              <Typography variant="body">{aboutHyblog.details}</Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={12}>
             <div style={{ maxWidth: `100px`, marginBottom: `1.45rem` }}>
               <Image />
             </div>
           </Grid>
-          <Grid item xs={12}>
-            <Typography variant="h6">
-              Hyreads is a social network architecture for the intellectual
-              space of our society, designed to bring together readers, writers,
-              the general intellectual community as well as the general public
-              to engage in activities engineered with the aim of facilitating
-              the sharing and broadcasting of ideas, heralding a search for
-              truth and paving the way towards the creation of a better society;
-              an Ideal society; a Just society.
-            </Typography>
-          </Grid>
         </Grid>
-        <Divider />
-        <List component="nav" className={classes.root}>
-          <ListItem
-            id="features"
-            button
-            onClick={() => handleClick('features')}
-          >
-            <ListItemIcon>
-              <StarIcon color="primary" />
-            </ListItemIcon>
-            <ListItemText primary="Hyreads Features" />
-            {features ? (
-              <ExpandMore color="primary" />
-            ) : (
-              <ExpandLess color="primary" />
-            )}
-          </ListItem>
-          <Collapse in={!features} timeout="auto" unmountOnExit>
-            <Accordion
-              expanded={expanded === 'panel1'}
-              onChange={handleChange('panel1')}
+        <Grid item xs={12} justify="center" align="center">
+          <Card raised>
+            <Typography variant="h4">{aboutHyreads.title}</Typography>
+            <Typography variant="body">{aboutHyreads.details}</Typography>
+          </Card>
+        </Grid>
+        <Card raised>
+          <List component="nav" className={classes.root}>
+            <ListItem
+              id="features"
+              button
+              onClick={() => handleClick('features')}
             >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1bh-content"
-                id="panel1bh-header"
+              <ListItemIcon>
+                <StarIcon color="primary" />
+              </ListItemIcon>
+              <ListItemText primary="Hyreads Features" />
+              {features ? (
+                <ExpandMore color="primary" />
+              ) : (
+                <ExpandLess color="primary" />
+              )}
+            </ListItem>
+            <Collapse in={!features} timeout="auto" unmountOnExit>
+              {aboutHyreads.features.map((feature) => {
+                return (
+                  <Accordion
+                    expanded={expanded === feature.title}
+                    onChange={handleChange(feature.title)}
+                    id={feature.caption}
+                  >
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls={feature.title}
+                      id={feature.title}
+                    >
+                      <Typography className={classes.heading}>
+                        {feature.title}
+                      </Typography>
+                      <Typography className={classes.secondaryHeading}>
+                        {feature.caption}
+                      </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Typography className={classes.description}>
+                        {feature.details}
+                      </Typography>
+                    </AccordionDetails>
+                  </Accordion>
+                )
+              })}
+            </Collapse>
+            <ListItem button onClick={() => handleClick('faqs')}>
+              <ListItemIcon>
+                <InfoIcon color="primary" />
+              </ListItemIcon>
+              <ListItemText primary="FAQs" />
+              {faqs ? (
+                <ExpandMore color="primary" />
+              ) : (
+                <ExpandLess color="primary" />
+              )}
+            </ListItem>
+            <Collapse in={!faqs} timeout="auto" unmountOnExit>
+              <Accordion
+                expanded={expanded === 'panel6'}
+                onChange={handleChange('panel6')}
               >
-                <Typography className={classes.heading}>Discourse</Typography>
-                <Typography className={classes.secondaryHeading}>
-                  Discuss the way forward
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography className={classes.description}>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Corrupti, voluptatum iste repellendus similique, error dicta
-                  tenetur ducimus tempore assumenda odit dolorem! Tempore sequi
-                  recusandae iusto minus eaque. Sit, obcaecati soluta.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion
-              expanded={expanded === 'panel2'}
-              onChange={handleChange('panel2')}
-            >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel2bh-content"
-                id="panel2bh-header"
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel6bh-content"
+                  id="panel6bh-header"
+                >
+                  <Typography className={classes.faqHeading}>
+                    What is Hyreads?
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography className={classes.description}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Corrupti, voluptatum iste repellendus similique, error dicta
+                    tenetur ducimus tempore assumenda odit dolorem! Tempore
+                    sequi recusandae iusto minus eaque. Sit, obcaecati soluta.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion
+                expanded={expanded === 'panel7'}
+                onChange={handleChange('panel7')}
               >
-                <Typography className={classes.heading}>Chatroom</Typography>
-                <Typography className={classes.secondaryHeading}>
-                  Connect with your contacts
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography className={classes.description}>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Corrupti, voluptatum iste repellendus similique, error dicta
-                  tenetur ducimus tempore assumenda odit dolorem! Tempore sequi
-                  recusandae iusto minus eaque. Sit, obcaecati soluta.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion
-              expanded={expanded === 'panel3'}
-              onChange={handleChange('panel3')}
-            >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel3bh-content"
-                id="panel3bh-header"
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel7bh-content"
+                  id="panel7bh-header"
+                >
+                  <Typography className={classes.faqHeading}>
+                    How do I share my ideas?
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography className={classes.description}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Corrupti, voluptatum iste repellendus similique, error dicta
+                    tenetur ducimus tempore assumenda odit dolorem! Tempore
+                    sequi recusandae iusto minus eaque. Sit, obcaecati soluta.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion
+                expanded={expanded === 'panel8'}
+                onChange={handleChange('panel8')}
               >
-                <Typography className={classes.heading}>Journals</Typography>
-                <Typography className={classes.secondaryHeading}>
-                  Read on from the greatest philosophers
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography className={classes.description}>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Corrupti, voluptatum iste repellendus similique, error dicta
-                  tenetur ducimus tempore assumenda odit dolorem! Tempore sequi
-                  recusandae iusto minus eaque. Sit, obcaecati soluta.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion
-              expanded={expanded === 'panel4'}
-              onChange={handleChange('panel4')}
-            >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel4bh-content"
-                id="panel4bh-header"
-              >
-                <Typography className={classes.heading}>Hyblog</Typography>
-                <Typography className={classes.secondaryHeading}>
-                  Keep up with our daily articles
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography className={classes.description}>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Corrupti, voluptatum iste repellendus similique, error dicta
-                  tenetur ducimus tempore assumenda odit dolorem! Tempore sequi
-                  recusandae iusto minus eaque. Sit, obcaecati soluta.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion
-              expanded={expanded === 'panel5'}
-              onChange={handleChange('panel5')}
-            >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel5bh-content"
-                id="panel5bh-header"
-              >
-                <Typography className={classes.heading}>Creators</Typography>
-                <Typography className={classes.secondaryHeading}>
-                  Express yourself uniquely
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography className={classes.description}>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Corrupti, voluptatum iste repellendus similique, error dicta
-                  tenetur ducimus tempore assumenda odit dolorem! Tempore sequi
-                  recusandae iusto minus eaque. Sit, obcaecati soluta.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-          </Collapse>
-          <ListItem button onClick={() => handleClick('faqs')}>
-            <ListItemIcon>
-              <InfoIcon color="primary" />
-            </ListItemIcon>
-            <ListItemText primary="FAQs" />
-            {faqs ? (
-              <ExpandMore color="primary" />
-            ) : (
-              <ExpandLess color="primary" />
-            )}
-          </ListItem>
-          <Collapse in={!faqs} timeout="auto" unmountOnExit>
-            <Accordion
-              expanded={expanded === 'panel6'}
-              onChange={handleChange('panel6')}
-            >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel6bh-content"
-                id="panel6bh-header"
-              >
-                <Typography className={classes.faqHeading}>
-                  What is Hyreads?
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography className={classes.description}>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Corrupti, voluptatum iste repellendus similique, error dicta
-                  tenetur ducimus tempore assumenda odit dolorem! Tempore sequi
-                  recusandae iusto minus eaque. Sit, obcaecati soluta.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion
-              expanded={expanded === 'panel7'}
-              onChange={handleChange('panel7')}
-            >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel7bh-content"
-                id="panel7bh-header"
-              >
-                <Typography className={classes.faqHeading}>
-                  How do I share my ideas?
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography className={classes.description}>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Corrupti, voluptatum iste repellendus similique, error dicta
-                  tenetur ducimus tempore assumenda odit dolorem! Tempore sequi
-                  recusandae iusto minus eaque. Sit, obcaecati soluta.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion
-              expanded={expanded === 'panel8'}
-              onChange={handleChange('panel8')}
-            >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel8bh-content"
-                id="panel8bh-header"
-              >
-                <Typography className={classes.faqHeading}>
-                  How about Hyblog?
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography className={classes.description}>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Corrupti, voluptatum iste repellendus similique, error dicta
-                  tenetur ducimus tempore assumenda odit dolorem! Tempore sequi
-                  recusandae iusto minus eaque. Sit, obcaecati soluta.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-          </Collapse>
-        </List>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel8bh-content"
+                  id="panel8bh-header"
+                >
+                  <Typography className={classes.faqHeading}>
+                    How about Hyblog?
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography className={classes.description}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Corrupti, voluptatum iste repellendus similique, error dicta
+                    tenetur ducimus tempore assumenda odit dolorem! Tempore
+                    sequi recusandae iusto minus eaque. Sit, obcaecati soluta.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </Collapse>
+          </List>
+        </Card>
       </div>
     </Layout>
   )
