feafa7bff439314fab90b4b93053896f464aec68
Jack Mkimbo <nickihll@gmail.com>
2021-04-18 05:19:20 +0300

    testing push notifications

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions')
 const Axios = require('axios')
+const admin = require('firebase-admin')
+admin.initializeApp()
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -21,4 +23,59 @@ exports.useWildcard = functions.firestore
         console.log('Gatsby build request successful sent')
       })
     }
+  })
+
+exports.sendNewPostNotification = functions.firestore
+  .document('fl_content/{fl_id}')
+  .onCreate(async (change, context) => {
+    let tokens = []
+    const snapshot = await admin.firestore().collection('subscriptions').get()
+    if (snapshot.empty) {
+      console.log('No tokens to send notifications to')
+    }
+    snapshot.forEach((doc) => {
+      // Listing all tokens as an array.
+      tokens.push(doc.data().token)
+    })
+    console.log(tokens)
+    // Notification details.
+    const post = await admin
+      .firestore()
+      .collection('fl_content')
+      .orderBy('_fl_meta_.createdDate', 'desc')
+      .limit(1)
+      .get()
+    const postData = post.docs[0].data()
+    const payload = {
+      notification: {
+        title: postData.title ? postData.title : postData.question,
+        body: postData.summary,
+        icon: `https://hyblog.info/assets/icon.png`,
+        fcm_options: {
+          link: `https://hyblog.info/${postData.slug}/`,
+        },
+      },
+    }
+    // Send notifications to all tokens.
+    const response = await admin.messaging().sendToDevice(tokens, payload)
+    // For each message check if there was an error.
+    const tokensToRemove = []
+    response.results.forEach((result, index) => {
+      const error = result.error
+      if (error) {
+        functions.logger.error(
+          'Failure sending notification to',
+          tokens[index],
+          error
+        )
+        // Cleanup the tokens who are not registered anymore.
+        if (
+          error.code === 'messaging/invalid-registration-token' ||
+          error.code === 'messaging/registration-token-not-registered'
+        ) {
+          tokensToRemove.push(snapshot.docs[index].ref.delete())
+        }
+      }
+    })
+    return Promise.all(tokensToRemove)
   })
