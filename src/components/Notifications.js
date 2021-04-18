ca31e072a113b36520a22e4d77efe0492f5e3790
Jack Mkimbo <nickihll@gmail.com>
2021-04-18 08:35:48 +0300

    edits

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -51,28 +51,32 @@ const askForPermissionToReceiveNotifications = () => {
     console.error(error)
   }
 }
-
-messaging.onMessage(function (payload) {
-  const notificationTitle = payload.notification.title
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.icon,
-  }
-
-  if (!('Notification' in window)) {
-    console.log('This browser does not support system notifications')
-  }
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === 'granted') {
-    // If it's okay let's create a notification
-    var notification = new Notification(notificationTitle, notificationOptions)
-    notification.onclick = function (event) {
-      event.preventDefault() // prevent the browser from focusing the Notification's tab
-      window.open(payload.notification.click_action, '_blank')
-      notification.close()
+useEffect(() => {
+  messaging.onMessage(function (payload) {
+    const notificationTitle = payload.notification.title
+    const notificationOptions = {
+      body: payload.notification.body,
+      icon: payload.notification.icon,
     }
-  }
-})
+
+    if (!('Notification' in window)) {
+      console.log('This browser does not support system notifications')
+    }
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === 'granted') {
+      // If it's okay let's create a notification
+      var notification = new Notification(
+        notificationTitle,
+        notificationOptions
+      )
+      notification.onclick = function (event) {
+        event.preventDefault() // prevent the browser from focusing the Notification's tab
+        window.open(payload.notification.click_action, '_blank')
+        notification.close()
+      }
+    }
+  })
+}, [])
 
 function Notifications() {
   return (
