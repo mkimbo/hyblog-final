d5dc7b34d7904ee8279d76415a0d8cd5bb99c9ea
Jack Mkimbo <jackmkimbo@gmail.com>
2021-07-29 01:01:22 +0300

    fixed error

--- src/pages/qa.js
+++ src/pages/qa.js
@@ -61,7 +61,6 @@ export const pageQuery = graphql`
                 fluid {
                   ...GatsbyImageSharpFluid
                 }
-                }
               }
             }
           }
